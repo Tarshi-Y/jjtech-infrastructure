75b75f12c39d6eac0767ab0f8fb58152de7071a5
eddison tarshi <eddybantar@gmail.com>
2022-12-13 20:50:34 -0500

    checkov scan fix

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   monitoring = true 
   ebs_optimized = true
+  root_block_device {
+      encrypted     = true
+ }
+   metadata_options {
+        
+       http_endpoint = "enabled"
+       http_tokens   = "required"
+  }
 
 
   tags = {
